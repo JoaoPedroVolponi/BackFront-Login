3bdb178b3364adb166db558810e9d2192f94bf88
João Pedro Volponi <jpvrosada@hotmail.com>
2024-01-16 21:31:09 -0300

    feat: 🎸 Adicionando camadas de arquitetura

---
+++ Login/Login/Screen/LoginScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreen.swift
+//  Login
+//
+//  Created by João Pedro Rosada Volponi on 16/01/24.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
