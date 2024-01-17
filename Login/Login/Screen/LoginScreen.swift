092442bac41d67b426b3d86e20cec4f01a5c53a5
João Pedro Volponi <jpvrosada@hotmail.com>
2024-01-16 21:41:04 -0300

    feat: 🎸 Separando Screen da ViewController

--- Login/Login/Screen/LoginScreen.swift
+++ Login/Login/Screen/LoginScreen.swift
@@ -9,12 +9,30 @@ import UIKit
 
 class LoginScreen: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "Login"
+        return label
+    }()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(self.loginLabel)
+        self.setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            self.loginLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.loginLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+        
 }
