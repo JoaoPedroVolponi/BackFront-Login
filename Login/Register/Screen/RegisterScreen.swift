cd55b3f58880cc7d849d26596977ace9d1da8841
João Pedro Volponi <jpvrosada@hotmail.com>
2024-01-29 22:19:23 -0300

    feat: 🎸 Adicionando botão de Cadastro

--- Login/Register/Screen/RegisterScreen.swift
+++ Login/Register/Screen/RegisterScreen.swift
@@ -51,6 +51,17 @@ class RegisterScreen: UIView {
         return tf
     }()
     
+    lazy var registerButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Cadastrar", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 7.5
+        button.backgroundColor = UIColor(red: 3/255, green: 58/255, blue: 51/255, alpha: 1.0)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.configBackGround()
